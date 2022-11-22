5e97cfa2f86c3dc0c9ee0785af119c58df220eff
douglasspeck <speck.empresarial@gmail.com>
2022-11-22 12:54:15 -0300

    Menu Documentation

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,15 +1,27 @@
+/**
+ * Toggle the visibility of the menu and animates its icon.
+ * 
+ * @access      public
+ * @author      Douglas Speck
+ * @since       0.1.0
+ * 
+ */
+
 function toggleMenu() {
 
+    // Get the elements within the DOM
     const menu = document.getElementById('menu');
     const svg = document.getElementById('menu-tag').children[0].children[0].children;
     
-    const tps = 30;
-    const tick = 1000 / tps;
-    const ticks = 18;
-    const deg = 45;
+    const tps = 30;             // 30 ticks per second
+    const tick = 1000 / tps;    // 1 tick = 1000/tps (ms)
+    const ticks = 18;           // Number of ticks in this animation
+    const deg = 45;             // Angle of rotation
 
+    // Check if the menu is opened
     var isOpened = menu.getAttribute('class') == 'open' ? true : false;
 
+    // If the menu is opened, close it
     if (isOpened) {
         menu.removeAttribute('class');
         for (let i = 1; i <= ticks; i++) {
@@ -20,7 +32,10 @@ function toggleMenu() {
             }, tick * i);
         }
         isOpened = false;
-    } else {
+    }
+    
+    // If the menu is closed, open it
+    else {
         menu.setAttribute('class', 'open');
         for (let i = 1; i <= ticks; i++) {
             setTimeout(function () {
@@ -32,6 +47,7 @@ function toggleMenu() {
         isOpened = true;
     }
 
+    // Log the status of the menu
     var menuStatus = isOpened ? 'opened' : 'closed'
     console.log(`The menu is ${menuStatus}.`);
 
