3c61a7d88107d0de86592ab08f05b74b77ca50e8
douglasspeck <speck.empresarial@gmail.com>
2023-03-07 00:24:17 -0300

    Fixed Login Page Formatting

--- assets/php/login.php
+++ assets/php/login.php
@@ -10,7 +10,7 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <script src="https://kit.fontawesome.com/cf6fa412bd.js" crossorigin="anonymous"></script>
 </head>
-<body>
+<body id="login">
   <div class="container">
     <div class="buttonsForm">
       <div class="btnColor"></div>
