9505b5673d529fa7f9c1d1a56b709e3b4b3a61c9
Douglas Speck <speck.empresarial@gmail.com>
2023-04-05 18:25:13 -0300

    Database integration

--- assets/php/mysqli/db.php
+++ assets/php/mysqli/db.php
@@ -31,6 +31,7 @@ function fetchAll(mysqli $db, $table){
 function getSequence(mysqli $db, $notebook, $sequence){
     $data = [];
     $sql = "SELECT ('id_activity','parameters') FROM 'activities' WHERE 'notebook' = " . $notebook . " AND 'sequence' = " . $sequence;
+    echo $db;
     $results = $db->query($sql);
     echo $results;
     if ($results->num_rows > 0) {
