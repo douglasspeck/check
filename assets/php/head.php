909696fbd7ba0e3d6553305c4096dc1070e168d6
Douglas Speck <speck.empresarial@gmail.com>
2023-05-10 16:54:52 -0300

    Fixed stylesheets url

--- assets/php/head.php
+++ assets/php/head.php
@@ -47,12 +47,12 @@
             <!-- Stylesheets -->
             <link rel="preload" as="style" onload="this.remove();" href="';
             if ($isLocal === 0) {
-                echo '/~ra260138';
+                echo '/~fracoes';
             };
             echo '/assets/php/stylesheets.php?t=' . date('YmdHis') . '" type="text/css">
             <link rel="stylesheet" href="';
             if ($isLocal === 0) {
-                echo '/~ra260138';
+                echo '/~fracoes';
             };
             echo '/assets/php/stylesheets.php?t=' . date('YmdHis') . '" type="text/css">';
 
